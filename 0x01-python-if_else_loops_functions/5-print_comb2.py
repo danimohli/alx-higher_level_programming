fb55c8fb845638ad69039960479b991d31bc30be
danimohli <danimohlife@gmail.com>
2024-03-12 20:49:24 +0100

    py std compl with corr out put

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 for x in range(0, 100):
-    if x < 10:
-        print("{}{}, ".format(0, x), end=' ')
-    elif x > 9 and x < 99:
-        print("{}, ".format(x), end=' ')
-    else:
-        print("{}".format(x))
+    if x < 99:
+        print("{}{}, ".format(x // 10, x % 10), end=' ')
+
+print("{}{}".format(x // 10, x % 10))
