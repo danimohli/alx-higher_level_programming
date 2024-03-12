51f75b4b31794c5219e47ccb2933d307b640bb8a
danimohli <danimohlife@gmail.com>
2024-03-12 20:44:12 +0100

    range of number with compliant

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 for x in range(0, 100):
-    if x < 99:
+    if x < 10:
+        print("{}{}, ".format(0, x), end=' ')
+    elif x > 9 and x < 99:
         print("{}, ".format(x), end=' ')
     else:
         print("{}".format(x))
