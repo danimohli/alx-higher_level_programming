80b1ed292fd15ef0cc230b0b3b09e13e0dcf3e51
danimohli <danimohlife@gmail.com>
2024-03-13 05:04:45 +0100

    z - A

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for c in range(122, 96, -1):
-        print("{}".format(chr(c - 32)) if c % 2 else chr(c), end="")
+    print("{}".format(chr(c - 32)) if c % 2 else chr(c), end="")
