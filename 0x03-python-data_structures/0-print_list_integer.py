210297cab192b2f657636836c53b350386f5477c
danimohli <danimohlife@gmail.com>
2024-03-16 23:34:55 +0100

    print list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+
+    # access item of list
+    for x in my_list:
+        # output each item accesssed by x
+        print("{:d}".format(x))
