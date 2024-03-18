ccc87cdf46b89588b93dc36a23f8dc273f082764
danimohli <danimohlife@gmail.com>
2024-03-18 21:42:30 +0100

    max number

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,11 +3,11 @@
 # To determine Max in list
 def max_integer(my_list=[]):
 
-    maxx = my_list[0]
-
     # checking if list is empty
     if len(my_list) == 0:
         return None
+
+    maxx = my_list[0]
 
     # loop to check max number
     for x in my_list:
