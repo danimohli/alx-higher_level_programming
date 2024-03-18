59601162cbc238104fac9aaf31969260c4a1dd61
danimohli <danimohlife@gmail.com>
2024-03-18 23:21:01 +0100

    del at idx

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -6,7 +6,7 @@ def delete_at(my_list=[], idx=0):
     # check if num is within range
     # of list index
 
-    if idx > len(my_list) or idx < 0:
+    if idx > len(my_list) - 1 or idx < 0:
         return my_list
 
     del my_list[idx]
