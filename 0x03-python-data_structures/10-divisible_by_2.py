aea770c9b7e34cb4cc727b782c53a5df9ec5faf0
danimohli <danimohlife@gmail.com>
2024-03-18 21:59:25 +0100

    division of 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -6,8 +6,10 @@ def divisible_by_2(my_list=[]):
     # new list to save result on
     result = []
 
+    # loop true list
     for x in my_list:
         if x % 2 == 0:
             result.append(True)
-        result.append(False)
+        else:
+            result.append(False)
     return result
