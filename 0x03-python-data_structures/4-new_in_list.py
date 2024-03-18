32c335254a3db58d25540e05927eaed81dca437c
danimohli <danimohlife@gmail.com>
2024-03-18 11:57:59 +0100

    return new list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,12 +6,12 @@
 def new_in_list(my_list, idx, element):
 
     new = []
-    # compare len with idx
+    if my_list:
+        # compare len with idx
+        if idx < 0 or idx > len(my_list):
+            return my_list
 
-    if idx < 0 or idx > len(my_list):
-        return my_list
-
-    new = my_list[:]
-    new[idx] = element
+        new = my_list[:]
+        new[idx] = element
 
     return new
