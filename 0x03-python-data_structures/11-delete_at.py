e3066b12529a9c611afb624fe2f7193eb3f2a8ff
danimohli <danimohlife@gmail.com>
2024-03-18 23:13:50 +0100

    del at idx

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# del list item usin index
+def delete_at(my_list=[], idx=0):
+
+    # check if num is within range
+    # of list index
+
+    if idx > len(my_list) or idx < 0:
+        return my_list
+
+    del my_list[idx]
+
+    return my_list
