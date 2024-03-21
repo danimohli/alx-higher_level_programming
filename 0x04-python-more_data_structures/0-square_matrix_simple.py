d3b8a869504e0bf4127e08a81361c03f20e8bf83
danimohli <danimohlife@gmail.com>
2024-03-21 04:48:16 +0100

    sqr of list

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# function to find square root of matrix of list
+
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+
+    # copy matrix to modify
+    n_matrix = [n[:] for n in matrix]
+
+    for a in range(len(n_matrix)):
+        for b in range(len(n_matrix[a])):
+            n_matrix[a][b] = n_matrix[a][b] ** 2
+    return n_matrix
