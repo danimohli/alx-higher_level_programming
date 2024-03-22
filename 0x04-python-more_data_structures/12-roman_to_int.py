bd4d5fc9c32e9c31fe9491f70136acc725c3381d
danimohli <danimohlife@gmail.com>
2024-03-22 16:20:49 +0100

    translate romans to number

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,18 @@ def roman_to_int(roman_string):
     # checking if incoming arg is str
     if isinstance(roman_string, int) or roman_string is None:
         return 0
+    numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                'C': 100, 'D': 500, 'M': 1000}
 
+    tal = 0
+    value = 0
 
+    for x in roman_string[::-1]:
+        va = numerals[x]
+        if va < value:
+            tal -= va
+        else:
+            tal += va
+        value = va
 
-
+    return tal
