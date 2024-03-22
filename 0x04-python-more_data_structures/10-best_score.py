67af875566e0ddd0fea26d2b44282fb4c63b4e83
danimohli <danimohlife@gmail.com>
2024-03-22 10:52:22 +0100

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -7,7 +7,7 @@ def best_score(a_dictionary):
     key = ""
 
     # check if a_dictionary is Empty
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
 
     # Loop through dicts to obtain key and value
