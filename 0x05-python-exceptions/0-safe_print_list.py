a4744df3b7c769eb965685f3955d558fed72e0df
danimohli <danimohlife@gmail.com>
2024-04-02 15:59:45 +0100

    more edit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 
-# function that print nth x of list
+# function that output nth of x
+def safe_print_list(my_list=[], n=0):
 
-def safe_print_list(my_list=[], x=0):
-
-    if x == 0:
-        return 0
-    try:
-        for i in range(x):
-            print("{}".format(my_list[i]), end='')
-        print()
-    except IndexError:
-        print()
-        return i
-
-    return i+1
+    x = 0
+    for i in range(n):
+        try:
+            print("{}".format(my_list[i]), end="")
+            x += 1
+        except IndexError:
+            break
+    print("")
+    return (x)
