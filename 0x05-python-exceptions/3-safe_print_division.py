d989ec592837199a8468405257e88dc796f61243
danimohli <danimohlife@gmail.com>
2024-04-02 19:56:24 +0100

    safe division of 2 int

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# divide two interger and output result
+# using finally section of exception
+
+def safe_print_division(a, b):
+
+    num = 0
+    try:
+        num = a / b
+    except (TypeError, ZeroDivisionError):
+        num = None
+    finally:
+        print('Inside result: {}'.format(result))
+        return (result)
