86f7669554c465b31f6513c79cfb519bd1a46b9f
danimohli <danimohlife@gmail.com>
2024-04-03 04:30:34 +0100

    err print int

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,12 +3,12 @@
 # import sys for stderr output function
 import sys
 
-# print to stderr but similar to forth
-# (fift start from zero) func
-defsafe_print_integer_err(value):
+# print to stderr but similar to
+def safe_print_integer_err(value):
+
     try:
         print('{:d}'.format(value))
-    except (TypeError, ValueError) as ra:
-        sys.stderr.write('Exception: {}\n'.format(ra))
+    except (ValueError, TypeError) as te:
+        stderr.write('Exception: {}\n'.format(te))
         return (False)
     return (True)
