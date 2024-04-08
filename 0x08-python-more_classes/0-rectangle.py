c75b2acc88306bc100867114794a7b189b91e3a1
danimohli <danimohlife@gmail.com>
2024-04-08 22:45:17 +0100

    documented

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -2,6 +2,7 @@
 
 """This module defines a simple Rectangle class."""
 
+
 class Rectangle:
     """Represents a rectangle with width and height.
 
@@ -12,4 +13,3 @@ class Rectangle:
         None
     """
     pass
-
