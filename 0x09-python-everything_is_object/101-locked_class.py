125ef5c093219099244ae524ed62f575d795575c
danimohli <danimohlife@gmail.com>
2024-04-09 18:14:31 +0100

    last_name

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""LockedClass with condition"""
+
+
+class LockedClass:
+    """ raise error if attribute isn't first_name"""
+
+    def __setattr__(self, name, value):
+        if name != 'first_name':
+            raise AttributeError("'LockedClass' object has no attribute '{}'"
+                                 .format(name))
+        else:
+            self.__dict__[name] = value
