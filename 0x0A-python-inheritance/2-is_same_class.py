76f6768eb3c257dd1f24a7beb9f2555a27a5c109
danimohli <danimohlife@gmail.com>
2024-04-15 23:24:19 +0100

    same_class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" Attribute same class check """
+
+
+def is_same_class(obj, a_class):
+    """
+    Check if the object is exactly an instance of the specified class.
+
+    Args:
+    obj: Any object.
+    a_class: The class to compare against.
+
+    Returns:
+    True if obj is an instance of a_class, otherwise False.
+    """
+    return type(obj) == a_class
