01221cc21520ba23b56748b02a2e63c6cd212f3a
danimohli <danimohlife@gmail.com>
2024-04-16 00:50:16 +0100

    same_class_rewr

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-
-""" function same class reff """
+"""
+module that cointains function to check if an object is an instance of a class
+"""
 
 
 def is_same_class(obj, a_class):
-
-    """ retn obj type to cmpare with a class """
-    return (type(obj) == a_class)
+    """
+    object is the most base type class, a_class
+    cointain instance of the object class
+    """
+# class type(object)
+    if (type(obj) == a_class):
+        # the object is exactly an instance of the a_class
+        return True
+    # otherwise,
+    return False
