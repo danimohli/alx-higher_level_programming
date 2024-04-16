b4a732e8b4c4f8547e84e1dcc6b6e41cc14cef6a
danimohli <danimohlife@gmail.com>
2024-04-16 01:48:53 +0100

    sq

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,47 +1,64 @@
 #!/usr/bin/python3
-"""inheritor class"""
+"""
+Module for a BaseGeometry class
+"""
 
 
 class BaseGeometry:
-    """BASE Class"""
-
+    """
+    still empty, area not implemented
+    """
     def area(self):
-        raise Exception("area() is not implemented")
+        raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+        if type(value) != int:
+            raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError('{} must be greater than 0'.format(name))
 
 
 class Rectangle(BaseGeometry):
-    """Rect Class"""
-
+    """
+    rectangle clasee herencia from BaseGeometry
+    """
     def __init__(self, width, height):
-        super().__init__()
-        self.__width = 0
-        self.__height = 0
+        """
+        init constructor
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
-        return self.__width * self.__height
+        """ returns the rectangle area """
+        return (self.__width * self.__height)
 
     def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
+        """ string with the following description """
+        return ("[{}] {}/{}".format(
+                self.__class__.__name__, self.__width, self.__height))
 
 
 class Square(Rectangle):
-    """SQ Class"""
-
+    """
+    class Square that inherits from Rectangle
+    """
     def __init__(self, size):
+        """ constructor, init square """
+        if self.integer_validator("size", size) is None:
+            self.__size = size
+
         super().__init__(size, size)
-        self.__size = 0
-        self.integer_validator("size", size)
-        self.__size = size
 
     def __str__(self):
-        return f"[Square] {self.__size}/{self.__size}"
+        """returns the string representation of the object. This method is
+        called when print() or str() function is invoked on an object."""
+
+        return "[Square] {}/{}".format(self.__size, self.__size)
+
+    def area(self):
+        """Calculates the area of a square"""
+
+        return self.__size * self.__size
