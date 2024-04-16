7006b3e7a15cec56325c01a1ff2c5f44d1c9171d
danimohli <danimohlife@gmail.com>
2024-04-16 22:53:27 +0100

    dislreload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" serialization & deserialization work"""
+
+
+class Student:
+    """init"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            attrs = [attr for attr in dir(self) if not attr.startswith("__")
+                     and not callable(getattr(self, attr))]
+
+        json_dict = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                json_dict[attr] = getattr(self, attr)
+        return json_dict
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
