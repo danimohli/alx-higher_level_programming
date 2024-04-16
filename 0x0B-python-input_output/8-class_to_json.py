6dccc8cd6229e8f24617dfc11f75baf8a4cb4eb3
danimohli <danimohlife@gmail.com>
2024-04-16 22:15:55 +0100

    json_dict

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''move class to json'''
+
+
+def class_to_json(obj):
+    '''json claass'''
+
+    json_dict = {}
+    for attr_name in dir(obj):
+        if not attr_name.startswith("__") and not
+        callable(getattr(obj, attr_name)):
+            attr_value = getattr(obj, attr_name)
+            if isinstance(attr_value, (list, dict, str, int, bool)):
+                json_dict[attr_name] = attr_value
+    return json_dict
