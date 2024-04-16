96e796c72f42eaf58f0dfbe0ab481d6d2a3cc6e9
danimohli <danimohlife@gmail.com>
2024-04-16 22:37:45 +0100

    json_dict

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-""" class to json funct """
+"""
+Module that contain a function that returns
+the dictionary description for JSON of an object
+"""
 
 
 def class_to_json(obj):
-    """ class dict """
-    json_dict = {}
-    for attr_name in dir(obj):
-        if not attr_name.startswith("__") and not
-        callable(getattr(obj, attr_name)):
-            attr_value = getattr(obj, attr_name)
-            if isinstance(attr_value, (list, dict, str, int, bool)):
-                json_dict[attr_name] = attr_value
-    return json_dict
+    """
+    returns the dictionary descritpion
+    """
+    return obj.__dict__
