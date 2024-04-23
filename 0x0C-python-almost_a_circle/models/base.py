67dca971dd616ee574e406140d7a881b894e89cc
danimhli <danimhlife@gmail.com>
2024-04-23 02:47:46 +0100

    create_method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -52,3 +52,16 @@ class Base:
             return []
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''Returns an instance with all attributes already set'''
+        if cls.__name__ == 'Rectangle':
+            dummy_instance = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            dummy_instance = cls(1)
+        else:
+            raise NotImplementedError("only implemented for Rect & Sqr cls")
+
+        dummy_instance.update(**dictionary)
+        return dummy_instance
