5e60a5dea195614baa33b3c003bc85ecf2507a58
danimhli <danimhlife@gmail.com>
2024-04-23 01:26:32 +0100

    rectangle_clase

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,31 +1,15 @@
 #!/usr/bin/python3
 
-from models.base import Base  # Importing the Base class
+from models.base import Base
 """# models/rectangle.py"""
 
 
 class Rectangle(Base):
-    """
-    A class representing a rectangle.
+    """A class representing a rectangle."""
 
-    Attributes:
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
-        x (int): The x-coordinate of the rectangle's position.
-        y (int): The y-coordinate of the rectangle's position.
-    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """
-        Constructs a new Rectangle instance.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-            x (int, optional): The x-coordinate of the rectangle's position.
-            y (int, optional): The y-coordinate of the rectangle's position.
-            id (int, optional): The unique identifier of the rectangle.
-        """
+        """Constructs a new Rectangle instance."""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -34,68 +18,40 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """
-        int: The width of the rectangle.
-        """
+        """int: The width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """
-        Sets the width of the rectangle.
-
-        Args:
-            value (int): The new width value.
-        """
+        """Sets the width of the rectangle."""
         self.__width = value
 
     @property
     def height(self):
-        """
-        int: The height of the rectangle.
-        """
+        """int: The height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
-        Sets the height of the rectangle.
-
-        Args:
-            value (int): The new height value.
-        """
+        """Sets the height of the rec"""
         self.__height = value
 
     @property
     def x(self):
-        """
-        int: The x-coordinate of the rectangle's position.
-        """
+        """int: The x-coordinate of the rectangle's position."""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """
-        Sets the x-coordinate of the rectangle's position.
-
-        Args:
-            value (int): The new x-coordinate value.
-        """
+        """Sets the x-coordinate of the rectangle's position."""
         self.__x = value
 
     @property
     def y(self):
-        """
-        int: The y-coordinate of the rectangle's position.
-        """
+        """int: The y-coordinate of the rectangle's position."""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """
-        Sets the y-coordinate of the rectangle's position.
-
-        Args:
-            value (int): The new y-coordinate value.
-        """
+        """Sets the y-coordinate of the rectangle's position"""
         self.__y = value
