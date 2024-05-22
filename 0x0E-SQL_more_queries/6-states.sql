7a56e9421132ae1417ca9fd840848ab182df02bd
danimohli <danimohlife@gmail.com>
2024-05-22 23:04:09 +0100

    state

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- State table with hbtn Db :)
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+use hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS `states`
+(
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
+	name VARCHAR(256)
+);
