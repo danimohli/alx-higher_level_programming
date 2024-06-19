851a98e829036b59f8fb6b8f62eb56a6c4d9d472
danimohli <danimohlife@gmail.com>
2024-06-19 22:21:52 +0100

    argument value

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,11 +3,7 @@
 const argv = process.argv.slice(2);
 
 if (argv[0]) {
-  let idx = 0;
-  while (argv[idx]) {
-    console.log(argv[idx]);
-    idx++;
-  }
+	console.log(argv.join(' '));
 } else {
   console.log('No argument');
 }
