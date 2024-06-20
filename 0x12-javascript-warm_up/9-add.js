f5f149f5092205e9b6510c13342d6b1cbb1f7857
danimohli <danimohlife@gmail.com>
2024-06-20 01:50:06 +0100

    add function

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const argv = process.argv.slice(2);
+
+const a = parseInt(argv[0], 10);
+const b = parseInt(argv[1], 10);
+
+function add(a, b) {
+  return a + b;
+}
+if (!isNaN(a) && !isNaN(b)) {
+  console.log(add(a, b));
+} else {
+  console.log('NaN');
+}
