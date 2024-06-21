c4e52fa4dce37a5eb88923ca1dccf01fd276cd1e
danimohli <danimohlife@gmail.com>
2024-06-21 03:15:15 +0100

    rd & concat files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+// Get file from command-line arguments
+const [,, firstFile, secondFile, destinationFile] = process.argv;
+
+// Read first file
+fs.readFile(firstFile, 'utf8', (err, data1) => {
+  if (err) {
+    console.error(`Error reading file ${firstFile}:`, err);
+    return;
+  }
+
+  // Read second file
+  fs.readFile(secondFile, 'utf8', (err, data2) => {
+    if (err) {
+      console.error(`Error reading file ${secondFile}:`, err);
+      return;
+    }
+
+    // contents of both files
+    const concatenatedData = data1 + data2;
+
+    // Write file  to the final file
+    fs.writeFile(destinationFile, concatenatedData, 'utf8', (err) => {
+      if (err) {
+        console.error(`Error writing to file ${destinationFile}:`, err);
+        return;
+      }
+    });
+  });
+});
