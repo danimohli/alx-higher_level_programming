383283a82015e4ae1f8e5eff952bf8c3dead70ab
danimohli <danimohlife@gmail.com>
2024-06-23 16:50:02 +0100

    state with letter n

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,10 @@ def list_states_starting_with_N(username, password, db_name):
     """
     List all states with a name starting with 'N' from the database.
     """
-    # Connect to the database
+    
+    """
+    Connect to the database
+    """
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -19,7 +22,9 @@ def list_states_starting_with_N(username, password, db_name):
         db=db_name
     )
 
-    # Create a cursor object
+    """
+    Create a cursor object
+    """
     cur = db.cursor()
 
     """
@@ -29,23 +34,33 @@ def list_states_starting_with_N(username, password, db_name):
     cur.execute("SELECT id, name FROM states WHERE name
                 LIKE 'N%' ORDER BY id ASC")
 
-    # Fetch all the results
+    """
+    Fetch all the results
+    """
     results = cur.fetchall()
 
-    # Print the results
+    """
+    Print the results
+    """
     for row in results:
         print(row)
 
-    # Close the cursor and the connection
+    """
+    Close the cursor and the connection
+    """
     cur.close()
     db.close()
 
 
 if __name__ == "__main__":
-    # Get arguments from command line
+    """
+    Get arguments from command line
+    """
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    # Call the function to list states
+    """
+    Call the function to list states
+    """
     list_states_starting_with_N(username, password, db_name)
