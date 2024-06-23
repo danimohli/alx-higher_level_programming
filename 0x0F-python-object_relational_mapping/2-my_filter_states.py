4c740e8233475cf22c9a5f82bdcf449a2580c1a3
danimohli <danimohlife@gmail.com>
2024-06-23 22:46:39 +0100

    filter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,50 +1,38 @@
 #!/usr/bin/python3
 """
-Script to list states from the database hbtn_0e_0_usa
-where name matches the argume
+Script to list states from the database
+hbtn_0e_0_usa where name matches the argument, safe from SQL injections.
+
+Usage:
+    ./safe_list_states_with_name.py
+    <username> <password> <database> <state_name>
+
+Arguments:
+    <username>: MySQL username
+    <password>: MySQL password
+    <database>: Database name
+    <state_name>: State name to search for
+
+Requirements:
+    - Uses MySQLdb for database connectivity.
+    - Connects to a MySQL server
+    running on localhost at port 3306.
+    - Lists states where name matches
+    the argument in ascending order by id.
+    - Results are displayed as (id, name) tuples.
 """
 import sys
 import MySQLdb
 
 
-def list_states_by_name(username, password, db_name, state_name):
-    """
-    Fetches and prints states from the database where name
-    matches
-    """
-
-    Connect to Database
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
-    )
-
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-
-    """
-    Order Queir
-    """
-    query = "SELECT id, name FROM states WHERE name = '{}'
-    ORDER BY id ASC".format(state_name)
-
-    cur.execute(query)
-
-    results = cur.fetchall()
-
-    for row in results:
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-
     cur.close()
     db.close()
-
-
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    list_states_by_name(username, password, db_name, state_name)
