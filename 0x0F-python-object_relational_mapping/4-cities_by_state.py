8718d33cb248611be2b6ebb93f7a379ee861225e
danimohli <danimohlife@gmail.com>
2024-06-23 23:45:16 +0100

    by_state_city

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -20,42 +20,14 @@ import sys
 import MySQLdb
 
 
-def list_cities(username, password, db_name):
-    """
-    Fetches and prints all cities from the database.
-
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        db_name (str): Database name.
-
-    Returns:
-        None
-    """
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
-    )
-
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-
-    cur.execute("SELECT id, name FROM cities ORDER BY id ASC")
-
-    results = cur.fetchall()
-
-    for row in results:
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id""")
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-
     cur.close()
     db.close()
-
-
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    list_cities(username, password, db_name)
