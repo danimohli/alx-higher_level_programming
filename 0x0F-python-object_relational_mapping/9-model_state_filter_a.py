558a48a1729d4560ea6c583b021e3af29b1f3d79
danimohli <danimohlife@gmail.com>
2024-06-24 00:53:52 +0100

    filter all state

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Lists all State objects that contain the letter 'a'
+from the database hbtn_0e_6_usa.
+
+Usage:
+    ./filter_states_a.py <mysql_username> <mysql_password> <database_name>
+
+Arguments:
+    <mysql_username>: MySQL username
+    <mysql_password>: MySQL password
+    <database_name>: Name of the database
+
+Requirements:
+    - Uses SQLAlchemy for database connectivity.
+    - Connects to a MySQL server running on localhost at port 3306.
+    - Results are sorted in ascending order by states.id.
+    - Displays the states as shown in the example.
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(f'mysql+mysqldb://\
+                           {username}:{password}@localhost:3306/{db_name}')
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    states = session.query(State).filter(State.name.like('%a%')).
+    order_by(State.id).all()
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
