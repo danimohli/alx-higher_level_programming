016bd35d89e98a75d13725de90d7390ff0c75316
danimohli <danimohlife@gmail.com>
2024-06-23 17:34:10 +0100

    pertan chg

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,17 +1,66 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
+"""
+Script to list states from the database hbtn_0e_0_usa starting with 'N'.
+
+Usage:
+    ./list_states_with_n.py <username> <password> <database>
+
+Arguments:
+    <username>: MySQL username
+    <password>: MySQL password
+    <database>: Database name
+
+Requirements:
+    - Uses MySQLdb for database connectivity.
+    - Connects to a MySQL server running on localhost at port 3306.
+    - Lists states with names starting with 'N' in ascending order by id.
+    - Results are displayed as (id, name) tuples.
+"""
+
 import sys
+import MySQLdb
+
+
+def list_states_starting_with_N(username, password, db_name):
+    """
+    Fetches and prints states from the database starting with 'N'.
+
+    Args:
+        username (str): MySQL username.
+        password (str): MySQL password.
+        db_name (str): Database name.
+
+    Returns:
+        None
+    """
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    results = cur.fetchall()
+
+    for row in results:
+        print(row)
+
+    cur.close()
+    db.close()
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY states.id""")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    if len(sys.argv) != 4:
+        print("Usage: ./list_states_with_n.py <username> <password> <database>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    list_states_starting_with_N(username, password, db_name)
