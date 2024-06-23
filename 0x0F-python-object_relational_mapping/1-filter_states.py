335dc7a166f6ab01e00793dd6b0819d7e95cf135
danimohli <danimohlife@gmail.com>
2024-06-23 18:44:13 +0100

    list of state

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module of list of state that start with N
+All module comment
 """
 import sys
 import MySQLdb
@@ -11,9 +11,6 @@ def list_states_starting_with_N(username, password, db_name):
     List all states with a name starting with 'N' from the database.
     """
 
-    """
-    Connect to the database
-    """
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -22,44 +19,23 @@ def list_states_starting_with_N(username, password, db_name):
         db=db_name
     )
 
-    """
-    Create a cursor object
-    """
     cur = db.cursor()
 
-    """
-    Execute the query to fetch states with names starting with
-    'N' ordered by id
-    """
     cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-    """
-    Fetch all the results
-    """
     results = cur.fetchall()
 
-    """
-    Print the results
-    """
     for row in results:
         print(row)
 
-    """
-    Close the cursor and the connection
-    """
     cur.close()
     db.close()
 
 
 if __name__ == "__main__":
-    """
-    Get arguments from command line
-    """
+    
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    """
-    Call the function to list states
-    """
     list_states_starting_with_N(username, password, db_name)
