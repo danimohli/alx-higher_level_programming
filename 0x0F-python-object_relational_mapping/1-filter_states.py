43413cfe8069f5f9e963e0fb6a031ca8ee204e73
danimohli <danimohlife@gmail.com>
2024-06-23 18:38:52 +0100

    list of state

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,39 +1,19 @@
 #!/usr/bin/python3
 """
-Script to list states from the database hbtn_0e_0_usa starting with 'N'.
-
-Usage:
-    ./list_states_with_n.py <username> <password> <database>
-
-Arguments:
-    <username>: MySQL username
-    <password>: MySQL password
-    <database>: Database name
-
-Requirements:
-    - Uses MySQLdb for database connectivity.
-    - Connects to a MySQL server running on localhost at port 3306.
-    - Lists states with names starting with 'N' in ascending order by id.
-    - Results are displayed as (id, name) tuples.
+Module of list of state that start with N
 """
-
 import sys
 import MySQLdb
 
 
 def list_states_starting_with_N(username, password, db_name):
     """
-    Fetches and prints states from the database starting with 'N'.
-
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        db_name (str): Database name.
-
-    Returns:
-        None
+    List all states with a name starting with 'N' from the database.
     """
 
+    """
+    Connect to the database
+    """
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -42,21 +22,44 @@ def list_states_starting_with_N(username, password, db_name):
         db=db_name
     )
 
+    """
+    Create a cursor object
+    """
     cur = db.cursor()
+
+    """
+    Execute the query to fetch states with names starting with
+    'N' ordered by id
+    """
     cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
+    """
+    Fetch all the results
+    """
     results = cur.fetchall()
 
+    """
+    Print the results
+    """
     for row in results:
         print(row)
 
+    """
+    Close the cursor and the connection
+    """
     cur.close()
     db.close()
 
 
 if __name__ == "__main__":
+    """
+    Get arguments from command line
+    """
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
+    """
+    Call the function to list states
+    """
     list_states_starting_with_N(username, password, db_name)
