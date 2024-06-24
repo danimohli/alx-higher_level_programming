72fe3560fcaec24b437597bd4c19bbf00ea36a80
danimohli <danimohlife@gmail.com>
2024-06-24 15:51:04 +0100

    obj by state

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -19,30 +19,28 @@ Requirements:
     - Results are sorted in ascending order by cities.id.
 """
 
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from relationship_state import Base, State
-from relationship_city import City
-
 if __name__ == "__main__":
-    # Get arguments from command line
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
+    from sqlalchemy.engine import create_engine
+    from sqlalchemy.engine.url import URL
+    from sqlalchemy.orm import Session
+    from relationship_city import City
+    from relationship_state import Base, State
+    from sys import argv
 
-    # Create engine and connect to the MySQL database
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}')
+    db = {'drivername': 'mysql+mysqldb',
+          'host': 'localhost',
+          'port': '3306',
+          'username': argv[1],
+          'password': argv[2],
+          'database': argv[3]}
 
+    url = URL(**db)
+    engine = create_engine(url, pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    Session = sessionmaker(bind=engine)
-
-    session = Session()
-
-    cities = session.query(City).join(City.state).order_by(City.id).all()
-
-    for city in cities:
-        print(f"{city.id}: {city.name} -> {city.state.name}")
-
+    session = Session(engine)
+    for state in session.query(State):
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name))
     session.close()
