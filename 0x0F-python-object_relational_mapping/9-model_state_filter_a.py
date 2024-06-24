43013a749b3cd16678a8e2743353098e5e3e5f0b
danimohli <danimohlife@gmail.com>
2024-06-24 01:04:26 +0100

    filter all state start wit a

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,26 +1,12 @@
 #!/usr/bin/python3
-
 """
 Lists all State objects that contain the letter 'a'
-from the database hbtn_0e_6_usa.
-Usage:
-    ./filter_states_a.py <mysql_username> <mysql_password> <database_name>
-
-Arguments:
-    <mysql_username>: MySQL username
-    <mysql_password>: MySQL password
-    <database_name>: Name of the database
-
-Requirements:
-    - Uses SQLAlchemy for database connectivity.
-    - Connects to a MySQL server running on localhost at port 3306.
-    - Results are sorted in ascending order by states.id.
-    - Displays the states as shown in the example.
 """
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+
 
 if __name__ == "__main__":
     engine = create_engine(f'mysql+mysqldb://\
@@ -29,7 +15,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
 
     session = Session()
-
     states = session.query(State).filter(State.name.like('%a%')).
     order_by(State.id).all()
 
