84691510aba1a8a499d84cce837ad9984624a7a0
danimohli <danimohlife@gmail.com>
2024-06-24 01:55:40 +0100

    insert

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Lists all State objects that contain the letter 'a'
+from the database hbtn_0e_6_usa.
+
+Usage:
+    ./filter_states_a.py <mysql_username> <mysql_password> <database_name>
+
+Arguments:
+    <mysql_username>: MySQL username
+    <mysql_password>: MySQL password
+    <database_name>: Name of the database
+
+Requirements:
+    - Uses SQLAlchemy for database connectivity.
+    - Connects to a MySQL server running on localhost at port 3306.
+    - Results are sorted in ascending order by states.id.
+    - Displays the states as shown in the example.
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    new_instance = session.query(State).filter_by(name='Louisiana').first()
+    print(new_instance.id)
+    session.commit()
