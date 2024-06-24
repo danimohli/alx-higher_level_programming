02a450ee7710137108b3b13cdeaec576530ee42a
danimohli <danimohlife@gmail.com>
2024-06-24 05:33:09 +0100

    list state cities

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -30,8 +30,7 @@ if __name__ == "__main__":
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    engine = create_engine(f'mysql+mysqldb://{username}:\
-            {password}@localhost:3306/{db_name}')
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}')
 
     Base.metadata.create_all(engine)
 
