92ba092df251231471509b3024072e6b3d02378f
danimohli <danimohlife@gmail.com>
2024-06-24 04:52:39 +0100

    update id

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Changes the name of a State object from the database hbtn_0e_6_usa.
+
+Usage:
+    ./update_state.py <mysql_username> <mysql_password> <database_name>
+
+Arguments:
+    <mysql_username>: MySQL username
+    <mysql_password>: MySQL password
+    <database_name>: Name of the database
+
+Requirements:
+    - Uses SQLAlchemy for database connectivity.
+    - Connects to a MySQL server running on localhost at port 3306.
+    - Changes the name of the State where id = 2 to 'New Mexico'.
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(f'mysql+mysqldb://{username}:{pass\
+            word}@localhost:3306/{db_name}')
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    state = session.query(State).filter(State.id == 2).first()
+
+    if state:
+        state.name = "New Mexico"
+        session.commit()
+
+    session.close()
