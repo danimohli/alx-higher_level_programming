f7ba4915606959275c673d450de1665631fe89e3
danimohli <danimohlife@gmail.com>
2024-07-04 14:53:22 +0100

    complied

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Use curl to get the size of the response body
-
-SIZE=$(curl -s -o /dev/null -w "%{size_download}" "$URL")
-
-echo "$SIZE"
+echo $(curl -s -o /dev/null -w "%{size_download}" "$URL")
