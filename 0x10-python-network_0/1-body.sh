47b738698901a01f83b41ff8eb26a601d0642ea1
danimohli <danimohlife@gmail.com>
2024-07-04 15:25:13 +0100

    get request

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script takes URL, sends a GET request, displays the body of a 200 status code
-if [ "$(curl -s -o /dev/null -w '%{http_code}' "$1")" -eq 200 ]; then curl -s "$1"; fi
+if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
