e8dc373fad9201c807e36216e0e1dfe2f0e5882f
danimohli <danimohlife@gmail.com>
2024-07-30 13:42:39 +0100

    write to file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const filePath = process.argv[2];
-const content = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log('File written successfully');
-});
-
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8',
+  function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
