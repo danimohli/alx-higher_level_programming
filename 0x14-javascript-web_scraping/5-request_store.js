95241876e8f1e4aa72d20a60871cef1e22b3efce
danimohli <danimohlife@gmail.com>
2024-07-30 16:12:08 +0100

    save url content

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+if (!url || !filePath) {
+  console.error('Usage: ./saveWebPage.js <URL> <file_path>');
+  process.exit(1);
+}
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  if (response.statusCode !== 200) {
+    console.error(`Failed to retrieve data. Status code: ${response.statusCode}`);
+    return;
+  }
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(`The content of ${url} has been saved to ${filePath}`);
+  });
+});
