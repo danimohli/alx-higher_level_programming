54355e736870ed1e5918eada88bccf54e5282cde
danimohli <danimohlife@gmail.com>
2024-07-30 14:11:04 +0100

    appearance time

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+const request = require('request');
+const apiUrl = process.argv[2];
+const characterId = 18;
+
+if (!apiUrl) {
+  console.error('Usage: ./countWedgeAntilles.js <API_URL>');
+  process.exit(1);
+}
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  if (response.statusCode !== 200) {
+    console.error(`Failed to retrieve data. Status code: ${response.statusCode}`);
+    return;
+  }
+  const films = JSON.parse(body).results;
+  let count = 0;
+
+  films.forEach((film) => {
+    if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+      count++;
+    }
+  });
+
+  console.log(count);
+});
