fab5e5cfe6f74ec9dda5fcc3556a6baff2f45de3
danimohli <danimohlife@gmail.com>
2024-08-07 16:34:38 +0100

    Hello, everyone

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,13 @@
+/* global $ */
+$(function () {
+  $('#btn_translate').click(function () {
+    const languageCode = $('#language_code').val();
+    const apiUrl = `https://www.fourtonfish.com/hellosalut/hello/?lang=${languageCode}`;
+
+    $.get(apiUrl, function (data) {
+      $('#hello').text(data.hello);
+    }).fail(function () {
+      $('#hello').text('Error fetching translation');
+    });
+  });
+});
