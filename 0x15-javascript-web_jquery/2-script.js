b58b5dc95df4cfca9f4b4a6c78e2ab457ed600ec
danimohli <danimohlife@gmail.com>
2024-08-07 15:53:38 +0100

    click to red

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,5 +1,6 @@
+/* global $ */
 $(document).ready(() => {
-    $('#red_header').click(() => {
-        $('header').css('color', '#FF0000');
-    });
+  $('#red_header').click(() => {
+    $('header').css('color', '#FF0000');
+  });
 });
