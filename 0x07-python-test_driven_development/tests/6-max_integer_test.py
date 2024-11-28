1a4c023b999c38f0b947c7b0aa5571d084cd5b03
danimohli <danimohlife@gmail.com>
2024-11-28 12:03:51 +0100

    test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,7 +8,8 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_positive_value(self):
         self.assertEqual(max_integer([9, 6, 10, 2]), 10)
-        self.assertEqual(max_integer([100, 0, 10.0, 78, 101]), 101)
+        self.assertEqual(max_integer([101, 0, 10.0, 78, 100]), 101)
+        self.assertEqual(max_integer([9]), 9)
 
     def test_negative_value(self):
         self.assertEqual(max_integer([-29, -76, -81, 0, -5]), 0)
@@ -17,3 +18,4 @@ class TestMaxInteger(unittest.TestCase):
     def test_float_value(self):
         self.assertEqual(max_integer([1.00, 0, 10.0, 78.4, 10.1]), 78.4)
         self.assertEqual(max_integer([-7.29, -7.6, -8.1, -4.5, -5.8]), -4.5)
+        self.assertEqual(max_integer([]), None)
